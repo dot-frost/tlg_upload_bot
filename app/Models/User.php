de5cf335adbb8cdb232602becf72b2154febb42d
Ali Talebi <talebia1993@gmail.com>
2022-12-05 09:46:54 +0330

    User model dissable increment primary key

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public $incrementing = false;
+
     public function files()
     {
         return $this->hasMany(File::class);
